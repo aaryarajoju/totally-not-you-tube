1195b6205ca1f806017a3d0fb5da68d1b77ea74a
AARYA RAJOJU <aaryarajoju@gmail.com>
2021-10-09 01:05:59 +0530

    component - comments

---
+++ src/components/comments/Comments.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import './_comments.scss';
+import Comment from '../comment/Comment';
+import { useDispatch, useSelector } from 'react-redux';
+import { addComment, getCommentsOfVideoById } from '../../redux/actions/comments.action';
+
+const Comments = ({ videoId, totalComments }) => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getCommentsOfVideoById(videoId));
+    }, [videoId, dispatch]);
+
+    const comments = useSelector(state => state.commentList.comments);
+
+    const [text, setText] = useState('');
+
+    const _comments = comments?.map(comment => comment.snippet.topLevelComment.snippet);
+
+    const handleComment = (e) => {
+        e.preventDefault();
+        if(text.length == 0) return;
+        dispatch(addComment(videoId, text));
+        setText('');
+    }
+
+    return (
+        <div className="comments">
+            <p>{totalComments} comments</p>
+            <div className="comments__form d-flex w-100 my-2">
+                <img src="https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png" alt="" className="rounded-circle mr-3"/>
+                <form onSubmit={handleComment} className="d-flex flex-grow-1">
+                    <input type="text" className="flex-grow-1" placeholder="write a comment" value={text} onChange={e => setText(e.target.value)} />
+                    <button className="border-0 p-2">Comment</button>
+                </form>
+            </div>
+
+            <div className="comments__list">
+                {
+                    _comments?.map((comment, i) => (
+                        <Comment comment={comment} key={i}/> 
+                    ))
+                }
+            </div>
+            
+        </div>
+    )
+};
+
+export default Comments;
